4ca21d067df5ebadd7866c802c6454816f861cf1
Ihor <ihor.chernykh@nure.ua>
2021-05-18 14:51:30 +0300

    Added new function sumOfDigits, and changed already written code.

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+// const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 document.querySelector('#calcBtn').addEventListener('click', function(event) {
     const birthdays = document.getElementsByName('dateOfBirth');
 
     const [firstBirthday, secondBirthday] = birthdays;
 
-    if (firstBirthday.value.length === 0 || secondBirthday.value.length === 0) {
-        console.error(`%cYou didn't enter the data.`, "color: green");
+    let firstDate = firstBirthday.value, secondDate = secondBirthday.value;
+
+    if (firstDate.length === 0 || secondDate.length === 0) {
+        console.error(`You did not enter the data.`);
     } else {
-        console.log(firstBirthday.value + ' ' + secondBirthday.value);
+        console.log(firstDate + ' ' + secondDate);
     }
+
+    let regExp = /^([0-2^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+
+    let checkFirstDate = regExp.test(firstDate), checkSecondDate = regExp.test(secondDate);
+
+    console.log(checkFirstDate + ' ' + checkSecondDate);
+
+    if (checkFirstDate && checkSecondDate) {
+        alert('Success.');
+
+        console.log(sumOfDigits(firstDate));
+    } else {
+        alert('Please, enter a valid data');
+    }
+
+
 });
 
 document.querySelector('#resetBtn').addEventListener('click', function(event) {
@@ -19,9 +39,8 @@ document.querySelector('#resetBtn').addEventListener('click', function(event) {
 });
 
 document.querySelector('#calcBtn2').addEventListener('click', function(event) {
-    const name = document.getElementsByName('userName');
+    const name = document.getElementById('userName');
 
-    console.log(name);
     if ( name.value.length === 0 ) {
         console.error(`%cYou didn't enter the data.`, "color: green");
     } else {
@@ -30,6 +49,40 @@ document.querySelector('#calcBtn2').addEventListener('click', function(event) {
 });
 
 document.querySelector('#resetBtn2').addEventListener('click', function(event) {
-    document.getElementsByName('userName').value = '';
+    document.getElementById('userName').value = '';
 });
+
+function sumOfDigits(dateOfBirth) {
+    const filteredArray = dateOfBirth.split('').filter((element) => element !== '.' && element !== '/');
+    const digits = filteredArray.map( (element) => +element );
+
+    let firstSum = digits.reduce( (total, current) => total + current, 0);
+    let lengthSum = firstSum.toString().length;
+
+    if (lengthSum > 1) {
+        let secondSum = firstSum.toString().split('').map((element) => +element).reduce( (total, current) => total + current, 0 );
+        let secondLength = secondSum.toString().length;
+
+        if (secondLength > 1) {
+            let thirdSum = secondSum.toString().split('').map((element) => +element).reduce( (total, current) => total + current, 0);
+            let thirdLength = secondSum.toString().length;
+
+            if (thirdLength > 1) {
+                let fourthSum = thirdSum.toString().split('').map((element) => +element).reduce( (total, current) => total + current, 0);
+
+                return fourthSum;
+            } else {
+                return thirdSum;
+            }
+
+        } else {
+            return secondSum;
+        }
+
+    } else {
+        return firstSum;
+    }
+
+    return filteredArray;
+}
 
